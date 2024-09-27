7a8437828fc9e9b69cf6d5701b1131cce4c9180f
kikilo123 <nikushabibileishvili91@gmail.com>
2024-09-27 18:50:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let wvero=document.querySelector(".wvero");
+let shearBro=document.querySelector(".shear-bro");
+let sheah=document.querySelector(".sheiar");
+let clickShear=document.querySelector(".sheiar i");
+
+clickShear.addEventListener("click",()=>{
+    shearBro.classList.toggle("active");
+    wvero.classList.toggle("active");
+    
+})
